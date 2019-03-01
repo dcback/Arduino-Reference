6c4540e1f87af7d6869b1fbe4585fc85ad664fee
dcback <45322614+dcback@users.noreply.github.com>
2019-03-02 08:47:30 +0900

    Create infiniteLoop_print.ino

---
+++ infiniteLoop/infiniteLoop_print.ino
@@ -0,0 +1,14 @@
+// Infinite Loop print for init(sensor...) 
+
+void setup() {
+    Serial.begin(9600);
+
+    if (true) {
+        while (1) { Serial.println("Failed to start sensor! Please check your wiring."); }
+    }
+}
+
+void loop() {
+    Serial.println("Success your system. ");
+    delay(1000);
+}
