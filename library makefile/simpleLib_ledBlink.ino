3d599d0e3843e8d0e8286bc3dc65b05d95c26be4
dcback <45322614+dcback@users.noreply.github.com>
2019-03-02 09:29:14 +0900

    Update simpleLib_ledBlink.ino

--- library makefile/simpleLib_ledBlink.ino
+++ library makefile/simpleLib_ledBlink.ino
@@ -1,1 +1,16 @@
+#include "simpleLED.h"  // Local library call 
 
+simpleLED cTRL;         // for function parameter in loop..
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    if ( cTRL.Switch(2) ) {         // Switch(int digitalPin) for Ctrl
+        cTRL.LED(13, 10, 100);      // cTRL.LED( byte pin, int times, int onTime)
+    }
+    Serial.println(cTRL.Switch(2));
+}
