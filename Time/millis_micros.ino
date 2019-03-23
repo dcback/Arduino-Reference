afb6401f834e5a9d001273b481833de2beeb89cf
DC Back <45322614+dcback@users.noreply.github.com>
2019-03-24 07:38:28 +0900

    Create millis_micros.ino

---
+++ Time/millis_micros.ino
@@ -0,0 +1,33 @@
+/********************************************************************
+    [ delay Function for No Temporarily stop the program ]
+    unsigned long preMillis = 0;
+    void loop() {
+    unsigned long curMillis = millis();
+    if ( curMillis - preMillis >= 1000 ) { => Action(); delay(1000);
+            preMillis = curMillis;
+            Action();                   }        
+*********************************************************************/
+#define ledPin      13
+
+unsigned long preMicros = 0;
+unsigned long preMillis = 0;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+    unsigned long curMicros = micros();
+    unsigned long curMillis = millis();
+
+    if ( curMicros - preMicros >= 500000 ) {    // delay(500);
+        preMicros = curMicros;
+        digitalWrite(ledPin, HIGH);
+
+        if ( curMillis - preMillis >= 1000 ) {  // delay(1000);
+            preMillis = curMillis;
+            digitalWrite(ledPin, LOW);
+        }
+    }
+}
