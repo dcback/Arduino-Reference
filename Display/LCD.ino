60e8387b167fbfc8316df42e79d0df4037e10eb5
DC Back <45322614+dcback@users.noreply.github.com>
2019-03-30 15:33:08 +0900

    Update and rename LCD to LCD.ino

---
+++ Display/LCD.ino
@@ -0,0 +1,27 @@
+/***************************************
+        Arduino ESP8266  STM32F103
+    SDA    4       4        PB7
+    SCL    5       5        PB6
+****************************************/
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup() {
+    lcd.begin();        // LCD initialize
+    lcd.backlight();    // Backlight on
+
+    lcd.setCursor(3, 0);
+    lcd.print("Hello, world!");
+    lcd.setCursor(2, 1);
+    lcd.print("Ywrobot Arduino!");
+    lcd.setCursor(0, 2);
+    lcd.print("Arduino LCM IIC 2019");
+    lcd.setCursor(2, 3);
+    lcd.print("Power By Ec-yuan!");
+}
+
+void loop() {
+    // Nothing Absolutely Nothing!
+}
