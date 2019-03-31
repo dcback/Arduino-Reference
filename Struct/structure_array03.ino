b126293f977036125563065e7b01f5f6cf096255
DC Back <45322614+dcback@users.noreply.github.com>
2019-03-31 17:27:57 +0900

    Create structure_array03.ino

---
+++ Struct/structure_array03.ino
@@ -0,0 +1,45 @@
+/******************************************
+    typedef struct Name {
+        dataType1 name1;
+        dataType2 name2;
+             :
+    };
+    struct Name var[100];
+        var[0].name1 ~ var[99].name2 defines
+ ******************************************/
+#include <Printf.h>
+
+typedef struct _Point2D {
+    int x;
+    int y;
+} Point2D;
+
+void setup() {
+    Serial.begin(9600);
+    printfBegin();    // must use for <Printf.h>
+}
+
+void loop() {
+
+    Point2D p[10];
+
+    p[0].x = 10;
+    p[0].y = 20;
+
+    p[1].x = 30;
+    p[1].y = 40;
+
+    p[2].x = 50;
+    p[2].y = 60;
+
+    p[3].x = 70;
+    p[3].y = 80;
+
+    p[4].x = 90;
+    p[4].y = 100;
+
+    printf("%d ,%d ,%d ,%d ,%d\n", p[0].x, p[1].x, p[2].x, p[3].x, p[4].x);
+    printf("%d ,%d ,%d ,%d ,%d\n", p[0].y, p[1].y, p[2].y, p[3].y, p[4].y);
+
+    Serial.end();     // serial ending
+}
