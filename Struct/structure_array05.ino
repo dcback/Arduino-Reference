8fd966085c499277d3a3c7d988a70af0fbb2d534
DC Back <45322614+dcback@users.noreply.github.com>
2019-04-22 07:40:43 +0900

    Create structure_array05.ino

---
+++ Struct/structure_array05.ino
@@ -0,0 +1,24 @@
+typedef struct {
+    word sensor1;
+    word sensor2;
+    word sensor3;
+    word sensor4;
+    word sensor5;
+} SensorInfo;
+
+char msg[100];
+
+void setup() {
+    Serial.begin(115200);
+}
+
+void loop() {
+
+    SensorInfo sInfo;
+    sInfo.sensor1 = 9999; sInfo.sensor2 = 20; sInfo.sensor3 = 30; sInfo.sensor4 = 40; sInfo.sensor5 = 9999;
+
+    sprintf(msg, ">>>[SENSOR] %d , %d , %d , %d , %d", sInfo.sensor1, sInfo.sensor2, sInfo.sensor3, sInfo.sensor4, sInfo.sensor5);
+    Serial.println(msg);
+    Serial.println("------------------------");
+    delay(1000);
+}
